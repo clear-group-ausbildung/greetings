4ca4e96c6ee6b2975a54f275d244f8933cea9b72
Christian Dobert <chr.dobert@gmail.com>
2014-06-16 13:10:20 +0200

    Fixes #31

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610164203) do
+ActiveRecord::Schema.define(version: 20140616110455) do
 
   create_table "appointments", force: true do |t|
     t.date     "begin_date"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20140610164203) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "name",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
