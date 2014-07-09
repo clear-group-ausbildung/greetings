fbf3bcb370b917ecb9a749e926bd767ce2bf4395
Christian Dobert <chr.dobert@gmail.com>
2014-07-09 10:29:29 +0200

    Updated user configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user_emails = ['Juergen.Leger@clear-it.de', 'Sven.Czyperreck@clear-it.de', 'Sybille.Raithel@clear-it.de', 'Daniela.Fehn@clear-it.de']
+user_emails = ['welcome@clear-group.de']
 user_emails.each do |user_email|
 	user = User.new
 	user.email = user_email
-	user.password = "test1234"
-	user.password_confirmation = "test1234"
+	user.password = "welcome2clear"
+	user.password_confirmation = "welcome2clear"
 	user.save!
 end
 
-admin_emails = ['Admin@clear-it.de']
+admin_emails = ['admin@clear-group.de']
 admin_emails.each do |admin_email|
 	user = User.new
 	user.email = admin_email
