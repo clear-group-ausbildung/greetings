e15a27d68e9975aff4161d5fcf137176bb44ecd6
Christian Dobert <chr.dobert@gmail.com>
2014-07-10 20:03:33 +0200

    Reformatted controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-	before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-	protected
+  protected
 
-		def configure_permitted_parameters
-			devise_parameter_sanitizer.for(:sign_up) << :name
-   		devise_parameter_sanitizer.for(:account_update) << :name
- 		end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name
+      devise_parameter_sanitizer.for(:account_update) << :name
+    end
 
-		def disable_nav
-		  @disable_nav = true
-		end
+    def disable_nav
+      @disable_nav = true
+    end
 end
