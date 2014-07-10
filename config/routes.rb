db99ba5297d380e4a63728855b408180491dc81a
Christian Dobert <chr.dobert@gmail.com>
2014-07-10 21:32:37 +0200

    Fixes #53

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,15 @@ Rails.application.routes.draw do
   resources :appointments
 
   devise_for :users
-  root 'appointments#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'appointments#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
