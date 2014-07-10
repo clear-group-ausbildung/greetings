5dd92daf9dc445caf6bbaabcb43f95a64460a2fd
Christian Dobert <chr.dobert@gmail.com>
2014-07-10 19:56:00 +0200

    Reformatted javascripts

--- app/assets/javascripts/bootstrap_datepicker_and_customization.js
+++ app/assets/javascripts/bootstrap_datepicker_and_customization.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
-	$('.input-group.date').datepicker({
-    format: "dd.mm.yyyy",
-    todayBtn: "linked",
-    language: "de",
-    daysOfWeekDisabled: "0,6",
-    calendarWeeks: true,
-    autoclose: true,
-    todayHighlight: true
-	});
-	//$('.input-group.date').datepicker('update', moment(get_url_param('begin_date')).format('DD.MM.YYYY'));
-});	
+  $('.input-group.date').datepicker({
+    format             : "dd.mm.yyyy",
+    todayBtn           : "linked",
+    language           : "de",
+    daysOfWeekDisabled : "0,6",
+    calendarWeeks      : true,
+    autoclose          : true,
+    todayHighlight     : true
+  });
+}); 
+
 function get_url_param(name) {
   name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
- 
-	var regexS = "[\\?&]"+name+"=([^&#]*)";
-	var regex = new RegExp( regexS );
-	var results = regex.exec( window.location.href );
+  
+  var regexS  = "[\\?&]"+name+"=([^&#]*)";
+  var regex   = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
 
-	if ( results == null )
-	  return "";
-	else
-	  return results[1];
+  if (results == null) {
+   return "";
+  } else {
+   return results[1];
+  }
 }
--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,46 +1,48 @@
 (function ($) {
-        $.fn.simpleClock = function () {
 
-        // Define weekdays and months
-        var weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
-        var months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+  $.fn.simpleClock = function () {
+    // Define weekdays and months
+    var weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+    var months   = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
 
-        // getTime - Where the magic happens ...
-        function getTime() {
-                var date = new Date(),
-                hour = date.getHours();
-                return {
-                        day: weekdays[date.getDay()],
-                        date: date.getDate(),
-                        month: months[date.getMonth()],
-                        hour: appendZero(hour),
-                        minute: appendZero(date.getMinutes()),
-                        second: appendZero(date.getSeconds())
-                };
-        }
+    // getTime - Where the magic happens ...
+    function getTime() {
+      var date = new Date(),
+      hour     = date.getHours();
+      return {
+        day    : weekdays[date.getDay()],
+        date   : date.getDate(),
+        month  : months[date.getMonth()],
+        hour   : appendZero(hour),
+        minute : appendZero(date.getMinutes()),
+        second : appendZero(date.getSeconds())
+      };
+    }
 
-        // appendZero - If the number is less than 10, add a leading zero.
-        function appendZero(num) {
-                if (num < 10) {
-                        return "0" + num;
-                }
-                return num;
-        }
+    // appendZero - If the number is less than 10, add a leading zero.
+    function appendZero(num) {
+      if (num < 10) {
+        return "0" + num;
+      }
+      return num;
+    }
 
-        // refreshTime - Build the clock.
-        function refreshTime() {
-                var now = getTime();
-                $('#date').html(now.day + ', ' + now.date + '. ' + now.month);
-                $('#time').html(now.hour + ":" + now.minute + ":" + now.second + " Uhr");
-        }
+    // refreshTime - Build the clock.
+    function refreshTime() {
+      var now = getTime();
+      $('#date').html(now.day + ', ' + now.date + '. ' + now.month);
+      $('#time').html(now.hour + ":" + now.minute + ":" + now.second + " Uhr");
+    }
 
-        // Tick tock - Run the clock.
-        refreshTime();
-        setInterval(refreshTime, 1000);
-        };
+    // Tick tock - Run the clock.
+    refreshTime();
+    setInterval(refreshTime, 1000);
+  
+  };
+
 })(jQuery);
 
 $(document).ready(function() {
-        $("#clock").simpleClock();
-        setInterval('updateClock()', 1000);
+  $("#clock").simpleClock();
+  setInterval('updateClock()', 1000);
 });
